e82b8db9de3a94003eb3eecafada840e37ad1712
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2023-03-19 17:13:55 +0100

    Add less restricitve patterns / Formatting

--- extension/core/core.js
+++ extension/core/core.js
@@ -1,19 +1,19 @@
 const matchPatternArray = [
-    new RegExp('meet.google.com/'),
-    new RegExp('teams.microsoft.com/'),
-    new RegExp('zoom.us/'),
-    new RegExp('meet.jit.si/'),
+  new RegExp("meet.google.com"),
+  new RegExp("teams.microsoft.com"),
+  new RegExp("zoom.us"),
+  new RegExp("meet.jit.si"),
 ];
 
 // match tab url against regex of given meeting patterns
-function isMeeting (tabUrl) {
-    for (let i = 0; i < matchPatternArray.length; i++) {
-        if (matchPatternArray[i].test(tabUrl)) {
-            return true;
-        }
+function isMeeting(tabUrl) {
+  for (let i = 0; i < matchPatternArray.length; i++) {
+    if (matchPatternArray[i].test(tabUrl)) {
+      return true;
     }
+  }
 
-    return false;
+  return false;
 }
 
 export { isMeeting };
